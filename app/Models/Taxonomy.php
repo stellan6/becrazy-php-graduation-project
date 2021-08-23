8a78e53251fd041237cd069c31c8b5ac8c664cd5
stellan6 <83956512+stellan6@users.noreply.github.com>
2021-08-23 21:26:48 +0900

    Taxonomyモデルを追加

---
+++ app/Models/Taxonomy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Taxonomy extends Model
+{
+    protected $table = 'taxonomy';
+
+    public function posts(){
+
+        return $this->belongsToMany('App\Models\Post','taxonomy_relationships');
+    }
+}
